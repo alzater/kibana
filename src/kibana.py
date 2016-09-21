7c80ad7a4267ba2eca9981a379762f5e36d18fb3
Alzater <alzater@mail.ru>
2016-09-21 22:45:39 +0300

    params

--- src/kibana.py
+++ src/kibana.py
@@ -5,67 +5,100 @@ from source_reader import SourceReader
 class Kibana:
     def __init__(self, log):
         self.log = log
-        self.products = []
-        self.dates = []
-        self.catalogues = {}
 
-        self.source_id_start = 0
-        self.source_limit_max = 10000
-        self.source_limit_min = 10
-
-        self.elastic_type = "log"
-
-        self.read_config()
+        if self.read_config() == False:
+            self.log("Error! Failed to load config! Exit!")
 
 
-    def get_elastic_index( self, date, product, catalogue ):
+    def get_elastic_index( self, date, product_name, catalogue ):
         return self.elastic_url + self.index_prefix + '-' +\
-               product + '-' + catalogue + '-' + date + '/'
+               product_name + '-' + catalogue + '-' + date + '/'
 
 
-    def get_elastic_index_type( self, date, product, catalogue ):
-        return self.get_elastic_index(date, product, catalogue) + self.elastic_type + '/' 
+    def get_elastic_index_type( self, date, product_name, catalogue ):
+        elastic_type = self.elastic_type.get(product_name)
+        if elastic_type == None:
+            elastic_type = self.elastic_type['default']
+        return self.get_elastic_index(date, product_name, catalogue) + elastic_type + '/' 
 
 
     def read_config(self):
         cfg = open('../config/kibana.cfg', 'r')
         cfg_data = json.loads(cfg.read())
 
-        self.source_url = cfg_data['source_url']
-        self.elastic_url = cfg_data['elastic_url']
-        self.elastic_type = cfg_data['elastic_type']
-        self.index_prefix = cfg_data['index_prefix']
-        self.need_recreate_index = cfg_data['recreate_index']
-        self.source_id_start = cfg_data['source_id_start']
-        self.source_limit_max = cfg_data['source_limit_max']
-        self.source_limit_min = cfg_data['source_limit_min']
-        self.catalogues = cfg_data['catalogues']
+        self.names = cfg_data.get('names')
+        if names == None:
+            return False
+        self.products = cfg_data.get('products')
+        if self.products == None:
+            return False
+        self.catalogues = cfg_data.get('catalogues')
+        if self.catalogues == None:
+            return False
+        self.source_server = cfg_data.get('source_server')
+        if self.server == None:
+            return False
+        self.source_params = cfg_data.get('source_params')
+        if self.source_params == None:
+            return False
+        self.params = cfg_data.get('params')
+        if self.params == None:
+            return False
+        self.logins = cfg_data.get('logins')
+        if self.logins == None:
+            return False
+        self.source_id_start = cfg_data.get('source_id_start')
+        if self.source_id_start == None:
+            return False
+        self.source_limit_max = cfg_data.get('source_limit_max')
+        if self.source_limit_max == None:
+            return False
+        self.source_limit_min= cfg_data.get('source_limit_min')
+        if self.source_limit_min == None:
+            return False
+        self.elastic_url = cfg_data.get('elastic_url')
+        if self.elastic_url == None:
+            return False
+        self.need_recreate_index = cfg_data.get('recreate_index')
+        if self.need_recreate_index == None:
+            return False
+        self.index_prefix = cfg_data.get('index_prefix')
+        if self.index_prefix == None:
+            return False
+        self.elastic_type = cfg_data.get('elastic_type')
+        if self.elastic_type == None:
+            return False
+        return True
+
             
 
-    def delete_index(self, date, product, catalogue):
+    def delete_index(self, date, product_name, catalogue):
         if self.need_recreate_index == False:
             self.log("RECREATE INDEX: false")
             return
 
         self.log("ELASTIC DELETE INDEX")
         try:
-            res = requests.delete(self.get_elastic_index(date, product, catalogue))
+            res = requests.delete(self.get_elastic_index(date, product_name, catalogue))
         except:
             self.log("DELETE INDEX ERROR!")
             return
         self.log(res.text)
 
 
-    def create_index(self, date, product, catalogue):
+    def create_index(self, date, product_name, catalogue):
         self.log("ELASTIC CREATE INDEX")
-
+        
+        elastic_type = self.elastic_type.get(product_name)
+        if elastic_type == None:
+            elastic_type = self.elastic_type['default']
         try:
-            res = requests.put(self.get_elastic_index(date, product, catalogue), '{       \
+            res = requests.put(self.get_elastic_index(date, product_name, catalogue), '{  \
                 "settings" : {                                                            \
                     "number_of_shards" : 1                                                \
                 },                                                                        \
                 "mappings" : {                                                            \
-                    "'+ self.elastic_type +'" :{                                          \
+                    "'+ elastic_type +'" :{                                               \
                         "properties" : {                                                  \
                             "event"     : { "type" : "string", "index" : "not_analyzed" },\
                             "fproduct"  : { "type" : "string", "index" : "not_analyzed" },\
@@ -89,55 +122,66 @@ class Kibana:
         self.log(res.text)
 
 
-    def recreate_index(self, date, product, catalogue):
-        self.delete_index(date, product, catalogue)
-        self.create_index(date, product, catalogue)
+    def recreate_index(self, date, product_name, catalogue):
+        self.delete_index(date, product_name, catalogue)
+        self.create_index(date, product_name, catalogue)
 
 
-    def import_data(self, date, product, source_id=None, recr_index=None):
+    def import_data(self, date, product_name, source_id=None, recr_index=None):
         if recr_index == None:
             recr_index = self.need_recreate_index
 
-        if self.catalogues.get(product):
-            catalogues = self.catalogues[product]
-        else:
+        catalogues = self.catalogues.get(product_name):
+        if catalogues == None:
             catalogues = self.catalogues['default']
 
         for catalogue in catalogues:
-            self.log("NEW FILL! date:"+date+" product:"+product+" catalogue:"+catalogue)
+            self.log("NEW FILL! date:"+date+" product_name:"+product_name+" catalogue:"+catalogue)
+            
             if recr_index:
-                self.recreate_index(date, product, catalogue)
+                self.recreate_index(date, product_name, catalogue)
 
             if source_id != None:
                 self.source_id = source_id
             else:
                 self.source_id = self.source_id_start
 
-            self.fill_data(date, product, catalogue)
+            self.fill_data(date, product_name, catalogue)
 
 
-    def fill_data(self, date, product, catalogue):
-        reader = SourceReader(date, product, catalogue, self.source_url, self.source_id)
-        reader.set_log(self.log)
-        reader.set_limit(self.source_limit_min, self.source_limit_max)
-
-        while True:
-            result = reader.next_bulk()
-            if result == None:
-                return
-
-            bulk = ""
-            for record in result:
-                bulk += '{"index":{}}\n'
-                bulk += record + '\n'
+    #def fill_data(self, date, product_name, catalogue):
+        params = self.params.get(product_name)
+        if params == None:
+            params = self.params['default']
+        
+        product = self.products.get(product_name)
+        if product == None:
+            product = product_name
             
-            url = self.get_elastic_index_type(date, product, catalogue) + "_bulk"
-            try:
-                response = requests.post(url, bulk)
-            except KeyboardInterrupt:
-                self.log("STOPPED")
-                return
-            except:
-                self.log("FAILED TO INSERT DATA IN KIBANA")
-                continue
+        for param in params:
+            url = self.source_server
+            
+            #reader = SourceReader(date, product, catalogue, self.source_url, self.source_id)
+            reader.set_log(self.log)
+            reader.set_limit(self.source_limit_min, self.source_limit_max)
+            
+            while True:
+                result = reader.next_bulk()
+                if result == None:
+                    return
+
+                bulk = ""
+                for record in result:
+                    bulk += '{"index":{}}\n'
+                    bulk += record + '\n'
+                
+                url = self.get_elastic_index_type(date, product_name, catalogue) + "_bulk"
+                try:
+                    response = requests.post(url, bulk)
+                except KeyboardInterrupt:
+                    self.log("STOPPED")
+                    return
+                except:
+                    self.log("FAILED TO INSERT DATA IN KIBANA")
+                    continue
 
