744a9e56a1cf195ac01f9c9090a6857c2f2bf160
Alzater <alzater@mail.ru>
2016-11-16 11:32:26 +0300

    minor

--- src/configurator.py
+++ src/configurator.py
@@ -28,26 +28,19 @@ class Configuretor:
 
 
     def _read_config(self):
+        print
 
 
     def _print_help(self):
         print
         
-
-    
-def createParser ():
-    parser = argparse.ArgumentParser()
-    parser.add_argument ('-n', '--name')
-    parser.add_argument ('-c', '--count', type=int, default=1)
  
-    return parser
- 
- 
-if __name__ == '__main__':
-    parser = createParser()
-    namespace = parser.parse_args(sys.argv[1:])
- 
-    print (namespace)
- 
-    for _ in range (namespace.count):
-        print ("Привет, {}!".format (namespace.name) )
+#  example
+#if __name__ == '__main__':
+#    parser = createParser()
+#    namespace = parser.parse_args(sys.argv[1:])
+# 
+#    print (namespace)
+# 
+#    for _ in range (namespace.count):
+#        print ("Hello, {}!".format (namespace.name) )
