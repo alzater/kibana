101f738bc70085a395a4049b625b5d0abd5d351d
Alzater <alzater@mail.ru>
2016-12-29 15:13:45 +0300

    wip: use rules for preprocess source fields

--- src/source_reader.py
+++ src/source_reader.py
@@ -66,26 +66,26 @@ class SourceReader:
         while not fullBulk:
             fullBulk = True           
             result = []
-            try:
-                for cur_row in data:
-                    lines += 1
-                    row = self._get_row( cur_row )
-                    if row == None:
-                        self.log("ERROR! Failed to get row.")
-                        continue
-                    if row == 'ignore':
-                        continue
-                    
-                    row['date'] = self.date
-                    row['datetime'] = self._get_datetime(row)
+            #try:
+            for cur_row in data:
+                lines += 1
+                row = self._get_row( cur_row )
+                if row == None:
+                    self.log("ERROR! Failed to get row.")
+                    continue
+                if row == 'ignore':
+                    continue
+                  
+                row['date'] = self.date
+                row['datetime'] = self._get_datetime(row)
 
-                    json_row = json.dumps(row)
+                json_row = json.dumps(row)
 
-                    result.append(json_row)
-            except:
-                self.log("EXCEPTION! Failed to get row. id=["+self.iter.get_str()+"]")
-                if lines != limit:
-                    fullBulk = False
+                result.append(json_row)
+            #except:
+            #    self.log("EXCEPTION! Failed to get row. id=["+self.iter.get_str()+"]")
+            #    if lines != limit:
+            #        fullBulk = False
                     
         if self.iter_type == 'HH:MM:SS':
             self.hour += 1
@@ -122,7 +122,7 @@ class SourceReader:
 
             limit = self.limit.get()
             self.limit.increase()
-            
+
             stream = StringIO.StringIO( response.text.encode('utf-8') )
             return csv.reader( stream, delimiter=',' ), limit
 
@@ -131,13 +131,15 @@ class SourceReader:
         if len(data) <= 0:
             self.log("ROW ERROR! empty")
             return None
-            
+
         if self.iter_type == 'id':
             self.iter.set(data[0])
         else:
             self.iter.set(data[1])
 
         row = self._init_row()
+        self._preprocess_row(row)
+
         i = 0
         while i < len(data):
             if self.first_row[i] == "fvar":
@@ -145,8 +147,6 @@ class SourceReader:
             else:
                 self._add_param(self.first_row[i], data[i], row)
             i += 1
-        
-        self._preprocess_row(row)
 
         return row
 
@@ -161,6 +161,7 @@ class SourceReader:
 
     def _parse_fvar(self, fvar, row):
         params = fvar.split('&')
+        print params
         for param in params:
             try:
                 key, value = param.split('=')
@@ -170,61 +171,11 @@ class SourceReader:
 
 
     def _add_param(self, key, value, obj):
-        if key == "fid":
-            value = int(value)
-        if key == "fevent":
-            key = "event"
-        elif key == "e":
-            key = "event"
-        elif key == "ed":
-            key = "event_detail"
-        elif key == "fevent_detail":
-            key = "event_detail"
-        elif key == "skip":
-            return
-        elif key == "fgamecode":
-            return
-        elif key == "fgametype":
-            return
-        elif key == "furi":
-            return
-        elif key == "os":
-            key = "fos"
-        elif key == "ts":
-            return
         elif key == "amount":
             value = int(value)
-        elif key == "famountusd":
-            value = float(value)/100 
-        elif key == "credits":
-            value = int(value)
-        elif key == "level":
-            value = int(value)
-        elif key == "rubies":
-            value = int(value)
-        elif key == "gold":
-            value = int(value)
-        elif key == "food":
-            value = int(value)
-        elif key == "fps":
-            value = float(value)
-        elif key == "exp":
-            value = int(value)
-        elif key == "avg_frame_time":
-            value = int(value)
-        elif key == "max_frame_time":
-            value = int(value)
-        elif key == "price":
-            value = float(value)
-        elif key == "amount_free":
-            value = int(value)
-        elif key == "amount_pay":
-            value = int(value)
-
-
         obj[key] = value
-        
-        
+
+
     def _get_url(self):
         params = "date=" + self.date + \
                  self.iter.get_param()+"&p="+self.product+\
@@ -233,13 +184,13 @@ class SourceReader:
             params += "&beg_time=" + str(self.beg_time)
         if self.end_time != None:
             params += "&end_time=" + str(self.end_time)
-            
+
         params = self._add_auth(params)
-        
+
         url = self.url + "&" + params
         return url
-        
-        
+
+
     def _add_auth(self, params):
         if self.login_data == None:
             return params
@@ -247,7 +198,7 @@ class SourceReader:
             return params
         if self.login_data.get('password') == None:
             return params
-            
+
         if self.auth == False:
             if self._auth() != True:
                 return params
@@ -258,37 +209,88 @@ class SourceReader:
         data.sort()
         data = ''.join(data)
         data += self.login_data['password']
-        
+
         sign = md5.new(data).hexdigest()
         params += '&sign=' + sign
-        
+
         return params
-        
-    
+
+
     def _auth(self):
         url = self.url.split('?')[0]
         url += '?method=auth'
         url += '&login=' + self.login_data['login']
         url += '&password=' + self.login_data['password']
-        
         try:
             response = requests.get(url)
         except:
             return False
-        
         return True
-        
-        
+
+
     def _init_row(self):
         row = {}
         row['name'] = ""
         row['comment'] = ""
         row['event_detail'] = ""
-
         return row
-        
+
+
     def _preprocess_row(self, row):
-        # Rule1: Count our profit
+        self._parse_event_rule(row)
+        self._parse_event_detail_rule(row)
+        self._parse_uid_rule(row)
+        self._delete_unused_params_rule(row)
+        self._rename_keys_rule(row)
+
+        self._count_profit_rule(row)
+        self._remove_test_users_rule(row)
+        self._parse_amount_rule(row)
+
+
+    def _parse_event_rule(self, row):
+        if row.get('fevent'):
+            row['event'] = row['fevent']
+            row.remove('fevent')
+        elif row.get('e'):
+            row['event'] = row['e']
+            row.remove('e')
+
+
+    def _parse_event_detail_rule(self, row)
+        if row.get("ed"):
+            row['event_detail'] = row['ed']
+            row.remove('ed')
+        elif row.get("fevent_detail"):
+            row['event_detail'] = row['fevent_detail']
+            row.remove('fevent_detail')
+
+
+    def _parse_uid_rule(self, row):
+        if key == "fid":
+            value = int(value)
+
+
+    def _delete_unused_params_rule(self, row):
+        if row.get('skip'):
+            row.remove('skip')
+        if row.get('fgamecode'):
+            row.remove('fgamecode')
+        if row.get('fgametype'):
+            row.remove('fgametype'):
+        if row.get('furi'):
+            row.remove('furi')
+        if row.get('ts'):
+            row.remove('ts')
+
+
+    def _rename_keys_rule(self, row):
+        if row.get('os'):
+            row['fos'] = row['os']
+            row.remove('os')
+
+
+    def _count_profit_rule(self, row):
         if row['event'] == 'deposit':
             if row['fproduct'] == 'glads2_vk':
                 row['profit'] = float(row['famount']) * 2.91
@@ -297,17 +299,46 @@ class SourceReader:
             if row['fproduct'] == 'glads2_mm':
                 row['profit'] = float(row['famount']) * 0.42
 
-        # Rule2: Remove test users
-        if row['fuid'] == 232946 or \
-           row['fuid'] == 257158814382854806 or \
-           row['fuid'] == 7255913029939607050 or \
-           row['fuid'] == 5819770344824394920 or \
-           row['fuid'] == 72213 or \
-           row['fuid'] == 32256090 or \
-           row['fuid'] == 14926854930002548513:
+
+    def _remove_test_users_rule(self, row):
+        if row['fuid'] == "232946" or \
+           row['fuid'] == "257158814382854806" or \
+           row['fuid'] == "7255913029939607050" or \
+           row['fuid'] == "5819770344824394920" or \
+           row['fuid'] == "72213" or \
+           row['fuid'] == "32256090" or \
+           row['fuid'] == "14926854930002548513":
             row = 'ignore'
-            
-            
-    
-        
+
+
+    def _parse_amount_rule(self, row):
+        #TODO
+
+    def _convert_values_rule(self, row):
+        if row.get('famountusd'):
+            row['famountusd'] = float(row['famountusd') / 100
+
+        self._convert_value_to_float(row, 'fps')
+        self._convert_value_to_float(row, 'price')
+
+        self._convert_value_to_int(row, 'credits')
+        self._convert_value_to_int(row, 'level')
+        self._convert_value_to_int(row, 'rubies')
+        self._convert_value_to_int(row, 'gold')
+        self._convert_value_to_int(row, 'food')
+        self._convert_value_to_int(row, 'exp')
+        self._convert_value_to_int(row, 'avg_frame_time')
+        self._convert_value_to_int(row, 'max_frame_time')
+        self._convert_value_to_int(row, 'amount_free')
+        self._convert_value_to_int(row, 'amount_pay')
+
+
+    def _convert_value_to_int(self, row, key):
+        if row.get(key):
+            row[key] = int(row[key)
+
+
+    def _convert_value_to_float(self, row, key):
+        if row.get(key):
+            row[key] = float(row[key)
 
